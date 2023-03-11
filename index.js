a5897b0604227cbc94024e240549af128e4ad26c
BouchraEm <b.elmorabit@hotmail.com>
2023-03-11 12:21:57 +0100

    made the game rock scissor paper

---
+++ index.js
@@ -0,0 +1,26 @@
+function computerPlay() {
+  const options = ["rock", "paper", "scissors"];
+  return options[Math.floor(Math.random() * options.length)];
+}
+console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  const options = {
+    rock: { win: "scissors", lose: "paper" },
+    paper: { win: "rock", lose: "scissors" },
+    scissors: { win: "paper", lose: "rock" },
+  };
+
+  if (playerSelection === computerSelection) {
+    return `It's a tie!`;
+  } else if (options[playerSelection].win === computerSelection) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+const playerSelection = `scissors`;
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
